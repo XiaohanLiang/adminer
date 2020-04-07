3fd918b31a5b7c7e59a7dd44977bae737ef5b90c
xiaohan.liang <Liangxiaohan95@gmail.com>
2020-04-07 14:49:26 +0800

    decrypt functions

--- plugins/bastion-jumper.php
+++ plugins/bastion-jumper.php
@@ -7,16 +7,42 @@
 
 class BastionJumper{
 
-    function __construct() {
-        $_GET['username']='root';
+    var $tokens;
+
+    function __construct($raw) {
+        $this->tokens = $raw;
     }
 
     function credentials() {
-            return array(SERVER, 'root','my-secret-pw');
+    
+        $args=decrypt($this->raw);
+        if ($args == false) {
+            return array('','','');
+        }
+
+        $username=$ret->{'bastionuser'};
+        $accountName=$ret->{'accountName'};
+        $instanceName=$ret->{'instanceName'};
+        $password=$args->{'user_password'};
+        
+        return array(
+            "127.0.0.1:9501", 
+            "$username/$accountName@$instanceName",
+            $password,
+        ); 
     }
 
     function login($login, $password) {
         return true;
+    }
+
+    function decrypt($raw) {
+        exec("./decrypt $raw",$ret,$code);
+        if ($code != 0) {
+            return false;
+        }
+        $opt=json_decode($ret[0]);
+        return $opt->{'Arg'};
     }
 
 }
